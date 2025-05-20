445dbae842b6ce876ec5c6de63f498c2b932edaf
AlexdeJesusFS <alexjesus0116@gmail.com>
2025-05-20 07:43:44 -0300

    nova fixture para criar banco sqlite em memória contextmanager para mockar dados para datetime de created_at e updated_at

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,12 @@
 # mypy: disable-error-code="no-untyped-def"
 
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import event
+from sqlmodel import Session, SQLModel, create_engine
 
 from src.app import app
 
@@ -9,3 +14,33 @@ from src.app import app
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    SQLModel.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    SQLModel.metadata.drop_all(engine)
+
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2025, 1, 1)):
+    def fake_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at') and hasattr(target, 'updated_at'):
+            target.created_at = time
+            target.updated_at = time
+
+    event.listen(model, 'before_insert', fake_time_hook)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
