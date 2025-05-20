a1c20af6d043d65c00abee614fe5c5f1ef0dc682
AlexdeJesusFS <alexjesus0116@gmail.com>
2025-05-20 07:48:19 -0300

    novo test para db com teste para criação de user

---
+++ tests/test_db.py
@@ -0,0 +1,25 @@
+# mypy: disable-error-code="no-untyped-def"
+
+from sqlmodel import select
+
+from src.models.user import User
+
+
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='Lex', password='s&cret', email='lex@mail.com'
+        )
+        session.add(new_user)
+        session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'Lex'))
+
+    assert user.dict() == {
+        'id': 1,
+        'username': 'Lex',
+        'password': 's&cret',
+        'email': 'lex@mail.com',
+        'created_at': time,
+        'updated_at': time,
+    }
