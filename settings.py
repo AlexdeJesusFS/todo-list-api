1714258cfbfe188fd48ad859a14d242849311b16
AlexdeJesusFS <alexjesus0116@gmail.com>
2025-06-05 18:08:46 -0300

    novas variáveis de ambiente definidas

--- settings.py
+++ settings.py
@@ -5,4 +5,10 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file='.env', env_file_encoding='utf-8'
     )
-    DATABASE_URL: str
+    # valores default definidos para evitar erro [call-arg] do mypy
+    # como valored de .env são definidos em tempo de execução são
+    # invisíveis para o mypy
+    DATABASE_URL: str = ''
+    SECRET_KEY: str = ''
+    ALGORITHM: str = ''
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
