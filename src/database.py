974299aba8cda8c8c7ebe1abda3e0d21bc3a0bbd
AlexdeJesusFS <alexjesus0116@gmail.com>
2025-06-05 18:13:50 -0300

    atualizado caminho para importação de `Settings`

--- src/database.py
+++ src/database.py
@@ -2,12 +2,12 @@
 
 from sqlmodel import Session, create_engine
 
-from settings import Settings
+from src.settings import Settings
 
 # evita erro Missing named argument "DATABASE_URL" for "Settings"Mypycall-arg
 # erro exigi  DATABASE_URL na inicialização da classe
 # apesar de não ser necessário
-engine = create_engine(Settings().DATABASE_URL)  # type: ignore[call-arg]
+engine = create_engine(Settings().DATABASE_URL)
 
 
 def get_session():
